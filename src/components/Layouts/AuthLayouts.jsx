fb7740cec9ae69ddc6dd3e146a7c07a3c81afa76
ndrvndr <andreavindra37@gmail.com>
2023-05-03 13:29:06 +0700

    code cleanup

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -1,11 +1,21 @@
+import MoonOutline from "../../assets/moon-outline.svg";
 import PropTypes from "prop-types";
-import Navbar from "../Fragments/Navbar";
 
 const AuthLayouts = (props) => {
   const { children } = props;
   return (
     <>
-      <Navbar />
+      <nav className="flex justify-center items-center w-screen h-20 bg-white drop-shadow-sm mb-7 px-[18px] right-0 left-0 fixed top-0">
+        <div className="flex w-screen justify-between">
+          <h1 className="font-extrabold h-fit self-end cursor-pointer">
+            Where in the world?
+          </h1>
+          <div className="flex h-fit items-center cursor-pointer">
+            <img src={MoonOutline} className="w-5 mr-3" />
+            <span className="font-semibold self-end text-sm">Dark Mode</span>
+          </div>
+        </div>
+      </nav>
       {children}
     </>
   );
