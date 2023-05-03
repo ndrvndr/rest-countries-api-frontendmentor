308377f980159924aab2ca59630e48b1d81fa78b
ndrvndr <andreavindra37@gmail.com>
2023-05-03 13:33:02 +0700

    setup detailCountry page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-// import AuthLayouts from "./components/Layouts/AuthLayouts";
+// import DetailCountry from "./pages/detailCountry";
 import HomePage from "./pages/home";
 
 function App() {
   return (
     <main className="flex flex-col justify-center items-center">
       <HomePage />
-      {/* <AuthLayouts /> */}
+      {/* <DetailCountry /> */}
     </main>
   );
 }
